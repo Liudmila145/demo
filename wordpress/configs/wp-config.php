0595877a6bc74031a0185818627d8b719ca1314d
Liudmila <liudmila.maksimchuk99@gmail.com>
2022-07-03 21:33:25 +0000

    Remove comment

--- wordpress/configs/wp-config.php
+++ wordpress/configs/wp-config.php
@@ -2,12 +2,6 @@
 /**
  * The base configuration for WordPress
  *
- * The wp-config.php creation script uses this file during the installation.
- * You don't have to use the web site, you can copy this file to "wp-config.php"
- * and fill in the values.
- *
- * This file contains the following configurations:
- *
  * * MySQL settings
  * * Secret keys
  * * Database table prefix
